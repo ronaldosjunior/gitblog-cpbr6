62552fe4c394663c0d1ce2826a324ea151b31c1e
Guilherme Ferreira <guilffer@gmail.com>
2013-01-27 21:21:32 -0200

    gh-pages configs

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/gitblog-cpbr6/"
+http_images_path = "/gitblog-cpbr6/images"
+http_fonts_path = "/gitblog-cpbr6/fonts"
+css_dir = "public/gitblog-cpbr6/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
